75030b7d46a775643b6c6d9614acedf490701f3d
Chris Banwarth <github@aprettycoolprogram.com>
2024-05-08 10:26:22 -0400

    [VERSION] 0.2.0
    Rewrite, but no new functionality

---
+++ src/Convert/Color.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Du.Convert
+{
+    public class Color
+    {
+        public static ConsoleColor ToConsoleColor(string color)
+        {
+            return color.ToLower() switch
+            {
+                "b"  => ConsoleColor.Black,
+                "u"  => ConsoleColor.Blue,
+                "c"  => ConsoleColor.Cyan,
+                "du" => ConsoleColor.DarkBlue,
+                "dc" => ConsoleColor.DarkCyan,
+                "da" => ConsoleColor.DarkGray,
+                "dg" => ConsoleColor.DarkGreen,
+                "dm" => ConsoleColor.DarkMagenta,
+                "dr" => ConsoleColor.DarkRed,
+                "dy" => ConsoleColor.DarkYellow,
+                "a"  => ConsoleColor.Gray,
+                "g"  => ConsoleColor.Green,
+                "m"  => ConsoleColor.Magenta,
+                "r"  => ConsoleColor.Red,
+                "w"  => ConsoleColor.White,
+                "y"  => ConsoleColor.Yellow,
+                _    => ConsoleColor.White,
+            };
+        }
+    }
+}
---
+++ src/WithCommandLine/DisplayText.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Du.WithCommandLine
+{
+    public static class DisplayText
+    {
+        public static void InColor(string bgrnd, string fgrnd, string msg)
+        {
+            Console.BackgroundColor = Du.Convert.Color.ToConsoleColor(bgrnd);
+            Console.ForegroundColor = Du.Convert.Color.ToConsoleColor(fgrnd);
+            Console.WriteLine(msg);
+            Console.ResetColor();
+        }
+    }
+}
